80ca9b89ff523bccad10797bee4f664b294bd6bd
vedesh <vedeshkulkarni24@gmail.com>
2026-07-27 23:04:12 +0530

    Added Gemini API key support

--- src/agent.py
+++ src/agent.py
@@ -3,13 +3,15 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from langgraph.prebuilt import create_react_agent
 from langgraph.checkpoint.memory import InMemorySaver
 from src.tools import get_weather
-
+import os
 from dotenv import load_dotenv
 load_dotenv()
 
 llm = ChatGoogleGenerativeAI(
     model="gemini-3.6-flash",
     temperature=0,
+    google_api_key=os.getenv("GOOGLE_API_KEY")
+
 )
 memory = InMemorySaver()
 agent = create_react_agent(
